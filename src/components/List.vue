6fbd6046fa278ac9791f5efbc90f2a305dfb3090
daviddubliakov <dubliakov1@gmail.com>
2021-12-31 15:38:05 +0200

    init commit

---
+++ src/components/List.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="list">
+    <li class="list-item" v-for="(item, index) in data" :key="index">
+      {{ item }}
+      <button class="btn danger" v-on:click="deleteElement(index)">Delete</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props: {
+    data: Array,
+  },
+  methods: {
+    deleteElement(elementIndex) {
+      this.$props.data.splice(elementIndex, 1);
+    },
+  },
+};
+</script>
---
+++ src/pages/Notes.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container pt-5">
+    <div class="card">
+      <h1>{{ title }}</h1>
+      <div class="form-control">
+        <input
+          type="text"
+          :placeholder="placeholderString"
+          :value="inputValue"
+          v-on:input="inputChangeHandler"
+          v-on:keypress.enter="addNewNote"
+        />
+      </div>
+      <button class="btn" v-on:click="addNewNote">Add note</button>
+      <hr />
+      <List :data="notes" />
+    </div>
+  </div>
+</template>
+
+<script>
+import List from "../components/List.vue";
+
+export default {
+  name: "Notes",
+  components: {
+    List,
+  },
+  data() {
+    return {
+      title: "Notes",
+      inputValue: "",
+      placeholderString: "Write a note title...",
+      notes: ["Note 1", "Note 2"],
+    };
+  },
+  methods: {
+    inputChangeHandler(event) {
+      this.inputValue = event.target.value;
+    },
+    addNewNote() {
+      this.notes.push(this.inputValue);
+      this.inputValue = "";
+    },
+  },
+};
+</script>
